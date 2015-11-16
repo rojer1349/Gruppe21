434c84f1c539c5af91a8322be795ea652dc3ddcb
R053NR07 <jan.bickel@openmailbox.org>
2015-11-16 13:07:25 +0100

    Add print

---
+++ print.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <title>WME Course Exercise HTML</title>
+  <meta name="description" content="WME Übung 1 Gruppe#21">
+  <meta name="keywords" content="HTML,CSS,JavaScript">
+  <meta name="author" content="Jan Bickel, Radzhiv Khayretdinov">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <script src="js/script.js"></script>
+</head>
+<body>
+<?php
+require_once("class/world_data_parser.php");
+$parser = new WorldDataParser();
+$d_array = $parser->parseCSV("res/world_data_v1.csv", 1000, ',');
+$parser->saveXML($d_array, "res/world_data.xml");
+echo $parser->printXML();
+?>
+</body>
+</html>
